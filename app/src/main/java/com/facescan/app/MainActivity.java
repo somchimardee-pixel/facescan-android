8e7fbdfe4787f55ceab5a3ba367b8be345e53f43
somchimardee-pixel <somchimardee@gmail.com>
2026-03-15 09:47:43 +0700

    Update MainActivity.java

--- app/src/main/java/com/facescan/app/MainActivity.java
+++ app/src/main/java/com/facescan/app/MainActivity.java
@@ -1,13 +1,14 @@
 package com.facescan.app;
 
-import android.app.Activity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
 }
